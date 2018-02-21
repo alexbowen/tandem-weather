39eee4f33051cf29ffcd7792fd58a95d1f7c1807
alexbowen <alexbowen123@googlemail.com>
2018-02-21 23:55:19 +0000

    create more robust test cases

--- src/components/helpers/gridData.test.js
+++ src/components/helpers/gridData.test.js
@@ -15,17 +15,13 @@ describe.only('Grid dataset helper functions', () => {
         expect(splitDatasetArray(data, 1).length).toBe(1)
         expect(splitDatasetArray(data, 5)[0]).toBe(Array.from(Array(40).keys()))
 
-        expect(splitDatasetArray(data, 2.5).length).toBe(3)
+        expect(splitDatasetArray(data, 3).length).toBe(3)
         expect(splitDatasetArray(data, 5)[0]).toBe(Array.from(Array(14).keys()))
         expect(splitDatasetArray(data, 5)[2].length).toBe(12)
-
-        expect(splitDatasetArray(data, 2.4).length).toBe(2)
-        expect(splitDatasetArray(data, 5)[0]).toBe(Array.from(Array(20).keys()))
-        expect(splitDatasetArray(data, 5)[2].length).toBe(20)
     })
 
     it('should handle incorrect parameters', () => {
         expect(splitDatasetArray('string', 5)).toBeFalsy()
-        expect(splitDatasetArray(data, 'string').length).toBe(1)
+        expect(splitDatasetArray(data, 'string')).toBe(data)
     })
 })
