4c821bdf5869e5949f546c24578123cde0463040
alexbowen <alexbowen123@googlemail.com>
2018-02-21 23:57:29 +0000

    add implementation for incorrect parameters

--- src/components/helpers/gridData.js
+++ src/components/helpers/gridData.js
@@ -1,1 +1,12 @@
-export const splitDatasetArray = (dataArray, numberOfRows) => []
+export const splitDatasetArray = (dataArray, numberOfRows) => {
+
+    if (!Array.isArray(dataArray)) {
+        return false
+    }
+
+    if (!Number.isInteger(numberOfRows) || typeof numberOfRows !== 'number') {
+        return dataArray
+    }
+
+    return []
+}
