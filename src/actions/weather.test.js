5065831dd0bf4d810acc9896b861d491e2098a13
alexbowen <alexbowen123@googlemail.com>
2018-02-22 04:31:27 +0000

    improve unit tests

--- src/actions/weather.test.js
+++ src/actions/weather.test.js
@@ -2,9 +2,11 @@ import { getForecast } from './weather';
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
 import { WEATHER_REQUEST, WEATHER_SUCCESS } from './weather'
+//see comment below
+import { ADD_MESSAGE } from './messages'
 
-const middlewares = [thunk];
-const mockStore = configureMockStore(middlewares);
+const middlewares = [thunk]
+const mockStore = configureMockStore(middlewares)
 
 const mockResponse = (status, statusText, response) => {
     return new window.Response(response, {
@@ -13,20 +15,44 @@ const mockResponse = (status, statusText, response) => {
         headers: {
             'Content-type': 'application/json'
         }
-    });
-};
+    })
+}
 
-it('calls request and success actions if the fetch response was successful', () => {
-    window.fetch = jest.fn().mockImplementation(() =>
-        Promise.resolve(mockResponse(200, null, '{"london":"hot"}')));
+describe('Weather API actions', () => {
+    it('should call request and success actions if the fetch response was successful', () => {
+        window.fetch = jest.fn().mockImplementation(() =>
+            Promise.resolve(mockResponse(200, null, '{"london":"hot"}')))
 
-    const store = mockStore({weather: {}})
+        const store = mockStore({weather: {}})
 
-    return store.dispatch(getForecast())
-        .then((forecast) => {
-            const expectedActions = store.getActions();
-            expect(expectedActions.length).toBe(2);
-            expect(expectedActions).toContainEqual({ type: WEATHER_REQUEST });
-            expect(expectedActions).toContainEqual({ type: WEATHER_SUCCESS, forecast: { "london": "hot" } });
-        })
-});
+        return store.dispatch(getForecast())
+            .then((forecast) => {
+                const expectedActions = store.getActions()
+                expect(expectedActions.length).toBe(2)
+                expect(expectedActions).toContainEqual({ type: WEATHER_REQUEST })
+                expect(expectedActions).toContainEqual({ type: WEATHER_SUCCESS, forecast: { "london": "hot" } })
+            })
+    })
+
+    it('should call request and message actions if the fetch response failed', () => {
+        window.fetch = jest.fn().mockImplementation(() => Promise.reject())
+
+        const store = mockStore({weather: {}})
+
+        return store.dispatch(getForecast())
+            .then((forecast) => {
+                const expectedActions = store.getActions()
+                expect(expectedActions.length).toBe(2)
+                expect(expectedActions).toContainEqual({ type: WEATHER_REQUEST })
+                /**
+                 * ADD_MESSAGE isnt mocked and it could/should be, but actually some
+                 * refactoring needed as the catch block in the action should dispatch
+                 * a WEATHER_ERROR action and the message should be handled elsewhere
+                 */
+                expect(expectedActions).toContainEqual({ type: ADD_MESSAGE, 'message' : {
+                    type: 'danger',
+                    text: 'Error with weather API request'
+                }})
+            })
+    })
+})
--- src/components/helpers/gridData.test.js
+++ src/components/helpers/gridData.test.js
@@ -4,18 +4,20 @@ describe('Grid dataset helper functions', () => {
 
     let data
 
+    const generateArray = size => Array.from(Array(size).keys())
+
     beforeEach(() => {
-        data = Array.from(Array(40).keys())
+        data = generateArray(40)
     })
 
     it('should format an array of data suitable to show in row format', () => {
         expect(splitDatasetArray(data, 5).length).toBe(5)
-        expect(splitDatasetArray(data, 5)[0]).toEqual(Array.from(Array(8).keys()))
+        expect(splitDatasetArray(data, 5)[0]).toEqual(generateArray(8))
         
         expect(splitDatasetArray(data, 1).length).toBe(1)
-        expect(splitDatasetArray(data, 1)[0]).toEqual(Array.from(Array(40).keys()))
+        expect(splitDatasetArray(data, 1)[0]).toEqual(generateArray(40))
 
-        //not implementing this due to time/complexity
+        // not implementing this due to time/complexity
         // expect(splitDatasetArray(data, 3).length).toBe(3)
         // expect(splitDatasetArray(data, 3)[0]).toEqual(Array.from(Array(14).keys()))
         // expect(splitDatasetArray(data, 3)[2].length).toBe(12)
