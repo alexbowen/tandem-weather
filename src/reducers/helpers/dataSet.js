35116c670760d329b6cc13a145ab9608f6b05600
alexbowen <alexbowen123@googlemail.com>
2018-02-23 21:30:11 +0000

    add implementation to pad results list

--- src/reducers/helpers/dataSet.js
+++ src/reducers/helpers/dataSet.js
@@ -1,5 +1,25 @@
-export const pad = ({list, interval, boundryFn}) => []
+/**
+ * @param {Array} list
+ * @param {Number} interval
+ * @param {Function} boundryFn 
+ */
+export const pad = ({list, interval}, boundryFn) => {
 
-export const integrity = (list, interval) => false
+    const index = list.findIndex(value => isBoundry(value, boundryFn))
 
+    return new Array(interval - index).fill(null)
+        .concat(list)
+        .concat(new Array(interval - (interval - index)).fill(null))
+}
+
+/**
+ * @param {Array} list
+ * @param {Number} interval
+ */
+export const integrity = ({list, interval}) => list.length % interval === 0
+
+/**
+ * @param {*} value 
+ * @param {Function} testFn 
+ */
 export const isBoundry = (value, testFn) => testFn(value)
--- src/reducers/helpers/dataSet.test.js
+++ src/reducers/helpers/dataSet.test.js
@@ -15,11 +15,11 @@ describe('Dataset', () => {
     })
 
     it('should have list that has a length that is a multiple of the interval', () => {
-        expect(integrity(dataSet.list, dataSet.interval)).toBe(true)
+        expect(integrity({...dataSet})).toBe(true)
     })
 
     it('should pad forecast array with empty elements that fall out side forecast period', () => {
-        const boundryFn = (value) => new Date(value).getHours() === 0
-        expect(pad({...dataSet, boundryFn})).toEqual([null, null, null, null].concat(dataSet.list).concat([null, null, null]))
+        const boundryFn = (value) => new Date(value.dt_txt).getHours() === 0
+        expect(pad({...dataSet}, boundryFn)).toEqual([null, null, null, null, null].concat(dataSet.list).concat([null, null, null]))
     })
 })
