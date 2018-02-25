264fabe41bea0b1419aa8e73ecd0da11a10da24a
alexbowen <alexbowen123@googlemail.com>
2018-02-25 20:02:17 +0000

    add backfill to results array if it doesnt contain full days

--- src/reducers/helpers/dataSet.js
+++ src/reducers/helpers/dataSet.js
@@ -5,13 +5,23 @@
  */
 export const pad = (list, interval, boundryFn) => {
 
+    //find index in result array that represents a boundry
     const index = list.findIndex(value => isBoundry(value, boundryFn))
 
     if (index === 0) {
         return list
     }
 
-    return new Array(interval - index).fill(null).concat(list)
+    const result = nullArray(interval - index).concat(list)
+
+    //add back fill to array if it doesnt represent complete days
+    let backFill = []
+
+    if (result.length % interval !== 0) {
+        backFill = nullArray(interval - (result.length % interval))
+    }
+
+    return result.concat(backFill)
 }
 
 /**
@@ -19,3 +29,8 @@ export const pad = (list, interval, boundryFn) => {
  * @param {Function} testFn 
  */
 export const isBoundry = (value, testFn) => testFn(value)
+
+/**
+ * @param {Integer} length 
+ */
+const nullArray = length => new Array(length).fill(null)
--- src/reducers/helpers/dataSet.test.js
+++ src/reducers/helpers/dataSet.test.js
@@ -1,21 +1,30 @@
-import { dataSet } from './dataSet.mock'
+import { dataSet1, dataSet2 } from './dataSet.mock'
 import { pad, isBoundry } from './dataSet'
 
 describe('Dataset', () => {
 
     it('should contain a list array', () => {
-        expect(dataSet.list).toBeDefined()
-        expect(Array.isArray(dataSet.list)).toBe(true)
+        expect(dataSet1.list).toBeDefined()
+        expect(Array.isArray(dataSet1.list)).toBe(true)
     })
 
     it('should contain an interval integer', () => {
-        expect(dataSet.interval).toBeDefined()
-        expect(typeof dataSet.interval).toBe('number')
-        expect(dataSet.interval % 1).toBe(0)
+        expect(dataSet1.interval).toBeDefined()
+        expect(typeof dataSet1.interval).toBe('number')
+        expect(dataSet1.interval % 1).toBe(0)
     })
 
     it('should pad forecast array with empty elements that fall out side forecast period', () => {
         const boundryFn = (value) => new Date(value.dt_txt).getHours() === 0
-        expect(pad(dataSet.list, dataSet.interval, boundryFn)).toEqual([null, null, null, null, null].concat(dataSet.list))
+
+        expect(pad(dataSet1.list, dataSet1.interval, boundryFn))
+            .toEqual([null, null, null, null, null].concat(dataSet1.list))
+    })
+
+    it('should back fill forecast array if it doesn\'t represent full day', () => {
+        const boundryFn = (value) => new Date(value.dt_txt).getHours() === 0
+
+        expect(pad(dataSet2.list, dataSet2.interval, boundryFn))
+            .toEqual([null, null, null, null, null, null, null].concat(dataSet2.list).concat([null]))
     })
 })
